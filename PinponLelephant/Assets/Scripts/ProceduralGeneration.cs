b1ff640aba2ae8006948aa04dd5b870dd329e10e
llaumegui <guillaumelef59@gmail.com>
2021-04-22 15:10:40 +0200

    ProceduralGeneration + Regulation

---
+++ PinponLelephant/Assets/Scripts/ProceduralGeneration.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+    [Header("Bounds")]
+    public Vector2 Bounds;
+    public float SizeSphereCast;
+    public LayerMask Ground;
+    public LayerMask House;
+
+    [Header("Instance")]
+    public List<GameObject> HouseInstances;
+    public int StartSpawns;
+
+    [Header("Debug")]
+    public float TimeSpawn;
+    public bool EnableDebug;
+
+    float _time;
+
+    Transform _houses;
+
+    private void Start()
+    {
+        if(StartSpawns>0)
+        {
+            for (int i = 0; i < StartSpawns; i++)
+            {
+                int tries = 0;
+                while (!GenerateHouse())
+                {
+                    tries++;
+                    if (tries == 3)
+                    {
+                        Debug.Log("unable to spawn");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(EnableDebug)
+        {
+            _time += Time.deltaTime;
+            if(_time>=TimeSpawn)
+            {
+                _time = 0;
+
+                int tries = 0;
+                while (!GenerateHouse())
+                {
+                    tries++;
+                    if (tries == 3)
+                    {
+                        Debug.Log("unable to spawn");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public bool GenerateHouse()
+    {
+        Rect spawnPlace = new Rect((transform.position.x - Bounds.x/2), (transform.position.z - Bounds.y/2), Bounds.x, Bounds.y);
+
+        Vector3 spawn = new Vector3(Random.Range(spawnPlace.xMin,spawnPlace.xMax),transform.position.y,Random.Range(spawnPlace.yMin,spawnPlace.yMax));
+
+        Ray ray = new Ray(spawn, Vector3.down);
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, Ground))
+            return false;
+        else
+        {
+            if (Physics.OverlapSphere(hit.point, SizeSphereCast, House).Length>0)
+            {
+                Debug.Log("Collision with another house");
+                return false;
+            }
+            else
+            {
+                PlaceHouse(hit.point);
+                return true;
+            }
+        }
+    }
+
+    void PlaceHouse(Vector3 SpawnPlace)
+    {
+        if(_houses == null)
+        {
+            _houses = new GameObject("Houses").GetComponent<Transform>();
+        }
+
+        Quaternion rot = Quaternion.Euler(0, Random.Range(-45, 45),0);
+
+        GameObject selected = HouseInstances[Random.Range(0, HouseInstances.Count)];
+
+        GameObject instance = Instantiate(selected, SpawnPlace, rot, _houses);
+        VillageManager.Instance.Houses.Add(instance.GetComponent<House>());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Bounds.x,0,Bounds.y));
+        
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position + Vector3.up * 10, SizeSphereCast);
+    }
+}
---
+++ PinponLelephant/Assets/Scripts/VillageManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageManager : MonoBehaviour
+{
+    public ProceduralGeneration HouseGenerator;
+
+    static VillageManager _instance;
+
+    public static VillageManager Instance { get; private set; }
+
+    public List<House> Houses;
+
+    [Header("Values")]
+    public int MaxHouses;
+    public float FirePercent;
+
+    [Header("Time")]
+    public Vector2 MinMaxTimeRoll;
+    public int TimeRollDivisions;
+    float _timeRoll;
+    float _timeForRoll;
+    float _timeForRegulation;
+    public float TimeRegulation;
+    float _divide;
+
+
+    [Header("Debug")]
+    [Range(0,1)]public float Happiness;
+    public bool EnableDebug;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+            Destroy(gameObject);
+
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        _timeRoll = MinMaxTimeRoll.y;
+        _divide = MinMaxTimeRoll.y / TimeRollDivisions;
+    }
+
+    private void Update()
+    {
+        _timeForRoll += Time.deltaTime;
+        _timeForRegulation += Time.deltaTime;
+
+        if(_timeForRoll>=_timeRoll)
+        {
+            RollFire();
+
+            _timeForRoll = 0;
+
+            if (_timeRoll > MinMaxTimeRoll.x)
+                _timeRoll -= _divide;
+        }
+
+        if(_timeForRegulation>=TimeRegulation)
+        {
+            _timeForRegulation = 0;
+
+            if (Houses.Count > MaxHouses)
+                RegulateVillage();
+
+            if(Houses.Count<MaxHouses)
+                RegulateVillage(false);
+        }
+    }
+
+    private void RegulateVillage(bool delete = true)
+    {
+        if(delete)
+        {
+            List<House> housesToDelete = new List<House>();
+            Houses.Shuffle();
+            for (int i = Houses.Count - 1; i >= 0; i--)
+            {
+                if (Houses.Count > MaxHouses)
+                {
+                    if (Houses[i].OnFire)
+                        continue;
+                    else
+                    {
+                        housesToDelete.Add(Houses[i]);
+                        Houses.RemoveAt(i);
+                    }
+                }
+            }
+
+            DestroyHouse(housesToDelete);
+        }
+        else
+        {
+            int newHouses = Random.Range(1, MaxHouses - Houses.Count);
+
+            for (int i =0;i<newHouses;i++)
+            {
+                int tries = 0;
+                while (!HouseGenerator.GenerateHouse())
+                {
+                    tries++;
+                    if (tries == 3)
+                    {
+                        Debug.Log("unable to spawn");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    void RollFire()
+    {
+        foreach(House house in Houses)
+        {
+            if (house.OnFire)
+                continue;
+            else
+            {
+                float fire = Random.Range(FirePercent, 100);
+                if (fire == FirePercent)
+                    house.OnFire = true;
+            }
+        }
+    }
+
+    public void DestroyHouse(List<House> houses)
+    {
+        foreach (House house in houses)
+            DestroyHouse(house);
+    }
+
+    public void DestroyHouse(House house)
+    {
+
+    }
+}
