3d6ff1353b064a1c1addf97eb089458f4bf6746b
BaptisteHollebecq <55829266+BaptisteHollebecq@users.noreply.github.com>
2021-04-22 15:12:04 +0200

    debut controller

---
+++ PinponLelephant/Assets/Script/CameraController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject CameraMain;
+    public float CameraSpeed = 5;
+    public float ZoomSpeed = .2f;
+    public float CameraMaxDistance = 100;
+    public float CameraMinDistance = 25;
+    public LayerMask GroundLayer;
+
+     public PinPon pinpon;
+
+    Camera cam;
+    Vector3 MoveDirection;
+    Vector3 CamDir;
+    float CameraDistance;
+    float zoom;
+
+    private void Awake()
+    {
+        pinpon = FindObjectOfType<PinPon>();
+
+        cam = Camera.main;
+        CameraDistance = Vector3.Distance(transform.position, CameraMain.transform.position);
+        CamDir = (CameraMain.transform.position - transform.position).normalized;
+
+        SetCameraDistance();
+    }
+
+    private void Update()
+    {
+        MoveDirection = Vector3.zero;
+        zoom = 0;
+
+        UserInputs();
+        Clicks();
+    }
+
+
+    private void LateUpdate()
+    {
+        Move();
+    }
+
+    private void Clicks()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, GroundLayer))
+            {
+                Debug.Log("touched ground");
+                pinpon.AddPath(hit.point);
+            }
+        }
+    }
+
+
+    private void Move()
+    {
+        transform.Translate(MoveDirection, Space.World);
+
+        CameraDistance += -zoom * ZoomSpeed;
+        CameraDistance = Mathf.Clamp(CameraDistance, CameraMinDistance, CameraMaxDistance);
+
+        SetCameraDistance();
+    }
+
+    private void UserInputs()
+    {
+        if (Input.GetButton("Left"))
+        {
+            MoveDirection.x = -CameraSpeed / 100;
+        }
+        if (Input.GetButton("Right"))
+        {
+            MoveDirection.x = CameraSpeed / 100;
+        }
+        if (Input.GetButton("Up"))
+        {
+            MoveDirection.z = CameraSpeed / 100;
+        }
+        if (Input.GetButton("Down"))
+        {
+            MoveDirection.z = -CameraSpeed / 100;
+        }
+
+        if (Input.GetAxis("Scroll") != 0)
+        {
+            zoom = Input.GetAxis("Scroll");
+        }
+    }
+
+    private void SetCameraDistance()
+    {
+        CameraMain.transform.position = transform.position + CamDir * CameraDistance;
+    }
+}
---
+++ PinponLelephant/Assets/Script/Cantine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cantine : MonoBehaviour
+{
+    PinPon pp;
+
+    bool feed = false;
+
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.tag == "Pinpon")
+        {
+            pp = other.GetComponent<PinPon>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Pinpon")
+        {
+            pp = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Pinpon")
+        {
+            if (pp != null)
+            {
+                if (!feed)
+                {
+                    feed = true;
+                    pp.Food += 0.01f;
+                    pp.Emotion += 0.01f;
+                    if (pp.Emotion > 1)
+                        pp.Emotion = 1;
+                    if (pp.Food > 1)
+                        pp.Food = 1;
+                    StartCoroutine(ResetFeed());
+                }
+            }
+        }
+    }
+
+    private IEnumerator ResetFeed()
+    {
+        yield return new WaitForSeconds(.2f);
+
+        feed = false;
+    }
+}
