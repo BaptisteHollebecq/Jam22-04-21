82e34875a498d765f723dd1d76d895de4710b861
BaptisteHollebecq <55829266+BaptisteHollebecq@users.noreply.github.com>
2021-04-22 17:16:36 +0200

    villageois

---
+++ PinponLelephant/Assets/Script/Villageois.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Villageois : MonoBehaviour
+{
+    bool moving = false;
+
+    private void Update()
+    {
+        if (!moving)
+        {
+            moving = true;
+            float randX = Random.Range(-1f, 1f);
+            float randZ = Random.Range(-1f, 1f);
+
+            Vector3 position = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
+
+            transform.DOMove(position, Vector3.Distance(transform.position, position)).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                moving = false;
+            });
+        }
+    }
+}
---
+++ PinponLelephant/Assets/Script/Wheel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    PinPon pp;
+
+    bool feed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pinpon")
+        {
+            pp = other.GetComponent<PinPon>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Pinpon")
+        {
+            pp = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Pinpon")
+        {
+            if (pp != null)
+            {
+                if (!feed)
+                {
+                    feed = true;
+                    pp.Water += 0.01f;
+                    if (pp.Water > 1)
+                        pp.Water = 1;
+                    StartCoroutine(ResetFeed());
+                }
+            }
+        }
+    }
+
+    private IEnumerator ResetFeed()
+    {
+        yield return new WaitForSeconds(.2f);
+
+        feed = false;
+    }
+}
